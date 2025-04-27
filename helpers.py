bcf50e60c4cec085ff3fa3e65717706b4f32c060
Anant-Navadiya <Anantnavadiya1530@gmail.com>
2025-04-27 17:14:12 +0530

    v1.1.0

--- helpers.py
+++ helpers.py
@@ -1,21 +1,20 @@
 import os
 
 def process_file_name(file_name):
-    """Extract folder, file and model names from kebab-case filename."""
+    """Extract folder, file, and model names from kebab-case filename."""
     pre, _ = os.path.splitext(file_name)
     split_name = pre.split('-')
     folder_name = split_name[0].capitalize()
 
     if len(split_name) > 1:
-        if split_name[1].isdigit():
-            file_name = folder_name + split_name[1].capitalize()
-        else:
-            file_name = split_name[1].capitalize()
+        # Join all parts after the first part and capitalize each one
+        file_name = "".join(part.capitalize() for part in split_name[1:])
     else:
         file_name = "Index"
 
     model_name = f"{file_name}Model"
     return pre, folder_name, file_name, model_name
+
 
 def set_content(app_name, folder_name, model_name):
     """Generates default content for .cshtml.cs files."""
